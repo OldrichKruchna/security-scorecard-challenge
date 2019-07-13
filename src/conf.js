9e0debb8fcb100511a2c92bd6ae6ec14bc691086
Oldrich Kruchna <oldrich.kruchna@sinnerschrader.com>
2019-07-13 13:20:47 +0200

    add the questions configuration

---
+++ src/conf.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        question: 'Who?',
+        key: 'who'
+    },
+    {
+        question: 'What?',
+        key: 'what'
+    },
+    {
+        question: 'When?',
+        key: 'when'
+    },
+    {
+        question: 'Where?',
+        key: 'where'
+    }
+];
